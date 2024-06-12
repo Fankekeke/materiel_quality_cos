ad8fbaee41b7c67c9296155223d1bf3b4d0f8055
悲伤的橘子树 <42531874+Fankekeke@users.noreply.github.com>
2024-06-12 19:46:15 +0800

    Delete frontend directory

--- frontend/src/utils/urlKey.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-const getUrlKey = (name) => {
-  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.href) || [, ""])[1].replace(/\+/g, '%20')) || null
-
-}
-
-export { getUrlKey }
